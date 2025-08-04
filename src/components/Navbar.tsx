31675711d81d86d21f0b92883e553f4ac669c6bb
nick withers <nickwithers26@gmail.com>
2025-08-03 17:53:59 -0700

    scrollbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,7 @@ import Link from 'next/link';
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-[#1f1f1f] text-white">
-    {/* className="sticky top-0 z-50 backdrop-blur-md backdrop-saturate-150 bg-[rgba(31,31,31,0.75)]" */}
+    <nav className="sticky top-0 z-50 backdrop-blur-md backdrop-saturate-150 bg-[rgba(31,31,31,0.75)]">
       <div className="max-w-5xl mx-auto px-6 py-6 flex justify-between items-center">
         <Link href="/" className="text-[#468be6] text-xl font-semibold">n_w</Link>
         <div className="flex space-x-6 text-md">
